563e15c0ccf33bab685531102614eaab9d72d3a6
Tokusei Noborio <tokusei@nyampass.com>
2011-10-23 11:07:24 +0900

    Implement aurhorizatoin url

--- src/twitter_post/config.clj
+++ src/twitter_post/config.clj
@@ -1,5 +1,5 @@
 (ns twitter-post.config)
 
-(def consumer-key "upY7uCtlMWE9qUqKVHn7A")
-(def consumer-secret "xnsJUbfaJeMqRvptjdj61T5xjTbtv54ldimsrzL50")
+(def consumer-key "<consumer-key>")
+(def consumer-secret "<consumer-secret>")
 
